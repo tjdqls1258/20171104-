c3eb2d4ea12dd76368c9df061e6b1e8a7932f09f
user <user@532-14>
2018-11-15 13:59:51 +0900

    03.게임 클래스 생성

--- main.cpp
+++ main.cpp
@@ -1,47 +1,17 @@
 #include <SDL.h>
+#include "Game.h"
 
-SDL_Window* g_pWindow = 0;
-SDL_Renderer* g_pRenderer = 0;
-bool g_bRunning = false;
-bool init(const char* title, int xpos, int ypos, int width, int height, int flags)
-{
-	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
-	{
-		g_pWindow = SDL_CreateWindow(title, xpos, ypos, width, height, flags);
-		if (g_pWindow != 0)
-		{
-			g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
-		}
-	}
-	else
-	{
-		return false;
-	}
-	return true;
-}
-
-void render()
-{
-	SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
-	SDL_RenderClear(g_pRenderer);
-	SDL_RenderPresent(g_pRenderer);
-}
+Game* g_game = 0;
 
 int main(int argc, char* args[])
 {
-	if (init("Chapter 1 : Setting up SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN))
+	g_game = new Game();
+	g_game->init("챕터 1", 100, 100, 680, 480, false);//windows창 생성
+	while (g_game->running())
 	{
-		g_bRunning = true;
+		g_game->handleEvents();//사용자 입력
+		g_game->update();//정보 업데이트
+		g_game->render();//정보를 바탕으로 그려줌
 	}
-	else
-	{
-		return 1;
-	}
-	while (g_bRunning)
-	{
-		render();
-	}
-	SDL_Delay(5000);
-	SDL_Quit();
 	return 0;
 }
