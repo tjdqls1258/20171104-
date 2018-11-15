16575c4ec83b2e6bfb46af2f3100c8d98bca55ae
BuildTools <unconfigured@null.spigotmc.org>
2018-11-15 19:23:05 +0900

    PP12.Vector2D

--- GameFramWork/Enemy.cpp
+++ GameFramWork/Enemy.cpp
@@ -7,9 +7,9 @@ Enemy::Enemy(const LoaderParams* pParams) :
 }
 void Enemy::update()
 {
-	GameObject::update();
-	m_y += 1;
-	//m_x += 1;
+	m_position.setX(m_position.getX() + 1);
+	m_position.setY(m_position.getY() + 1);
+
 	m_currentFrame = int(((SDL_GetTicks() / 100) % 6));
 }
 
--- GameFramWork/SDLGameObject.h
+++ GameFramWork/SDLGameObject.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "GameObject.h"
+#include "Vector2D.h"
 
 class SDLGameObject : public GameObject
 {
@@ -9,8 +10,10 @@ public:
 	virtual void update();
 	virtual void clean();
 protected:
-	int m_x;
-	int m_y;
+	Vector2D m_position;
+	Vector2D m_velocity;
+	Vector2D m_acceleration;
+
 	int m_width;
 	int m_height;
 	int m_currentRow;
